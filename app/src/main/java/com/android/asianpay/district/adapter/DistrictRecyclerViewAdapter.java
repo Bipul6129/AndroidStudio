89da9f8499d1a35476751f756242c5394d593103
Kushagra <kushagra.acharya@outlook.com>
2023-04-24 11:07:46 +0545

    district

---
+++ app/src/main/java/com/android/asianpay/district/adapter/DistrictRecyclerViewAdapter.java
@@ -0,0 +1,19 @@
+package com.android.asianpay.district.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DistrictRecyclerViewAdapter extends
+        RecyclerView.Adapter<DistrictRecyclerViewAdapter.ViewHolder>
+{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    //todo 1 implement methods and complete each function
+    //todo 2 create constructor and variables
+}
